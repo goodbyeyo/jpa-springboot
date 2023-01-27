9b8e5a9cb9ea2e3352a41796bca5b9398ab08782
Sangwook Hwang <yoonseol1983@gmail.com>
2023-01-28 00:32:13 +0900

    #1 - Spring Data REST + HAL Explorer
    간편한 조회 API 기능 구현을 위한 설정 및 구현
    어노테이션 추가 : @RepositoryRestResource
    application.yml : Data REST 설정 추가
    테스트 코드 추가 : Data REST Integration TEST
    build.gradle : rest-hal-explorer 추가

---
+++ board/src/test/java/com/spring/board/controller/DataRestTest.java
@@ -0,0 +1,73 @@
+package com.spring.board.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//@WebMvcTest
+@DisplayName("Data REST 테스트")
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest
+public class DataRestTest {
+
+    private final MockMvc mockMvc;
+
+    public DataRestTest(@Autowired MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @DisplayName("API 게시글 전체 조회")
+    @Test
+    void article_selectAll_test() throws Exception {
+        // given & when & then
+        mockMvc.perform(get("/api/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("API 게시글 단건 조회")
+    @Test
+    void article_selectOne_test() throws Exception {
+        // given & when & then
+        mockMvc.perform(get("/api/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("API 게시글의 댓글 조회")
+    @Test
+    void articleComment_select_test() throws Exception {
+        // given & when & then
+        mockMvc.perform(get("/api/articles/1/articleComments"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("API 댓글 전체 조회")
+    @Test
+    void articleComment_selectAll_test() throws Exception {
+        // given & when & then
+        mockMvc.perform(get("/api/articleComments"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("API 댓글 단건 조회")
+    @Test
+    void articleComment_selectOne_test() throws Exception {
+        // given & when & then
+        mockMvc.perform(get("/api/articleComments/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+}
