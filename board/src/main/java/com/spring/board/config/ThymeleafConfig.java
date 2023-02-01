2a688e686a49e9c0ce687f709f033f389b7c0273
Sangwook Hwang <yoonseol1983@gmail.com>
2023-02-01 23:49:26 +0900

    16 - 스프링
    * 사용자 정의 application property 를 IDE 에서 편리하게 사용하기 위한 Spring Boot Configuration Processor 활용
    * 참고 : https://docs.spring.io/spring-boot/docs/current/reference/html/application-properties.html
    * decoupled logic 반영한 thymeleaf3 설정 및 view 구현

---
+++ board/src/main/java/com/spring/board/config/ThymeleafConfig.java
@@ -0,0 +1,44 @@
+package com.spring.board.config;
+
+// import org.springframework.boot.context.properties.ConfigurationProperties;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+// import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+//import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+
+@Configuration
+// @EnableConfigurationProperties(ThymeleafConfig.class)
+public class ThymeleafConfig {
+
+    @Bean
+    // @ConditionalOnMissingBean   // Bean  없을 경우에만 Bean 등록 thymeleaf3Properties
+    public SpringResourceTemplateResolver thymeleafTemplateResolver(
+            SpringResourceTemplateResolver defaultTemplateResolver,
+            Thymeleaf3Properties thymeleaf3Properties
+    ) {
+        defaultTemplateResolver.setUseDecoupledLogic(thymeleaf3Properties.isDecoupledLogic());
+
+        return defaultTemplateResolver;
+    }
+
+
+    @RequiredArgsConstructor
+    @Getter
+    @ConstructorBinding   // 생성자 바인딩
+    @ConfigurationProperties("spring.thymeleaf3") // application.yml 에서 spring.thymeleaf3 를 찾아서 바인딩
+    public static class Thymeleaf3Properties {
+        /**
+         * Use Thymeleaf 3 Decoupled Logic
+         */
+        private final boolean decoupledLogic;
+
+    }
+}
