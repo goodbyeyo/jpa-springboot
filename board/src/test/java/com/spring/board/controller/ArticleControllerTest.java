04ce9657c599a9c14bd1abc6b5b9fba3488966f9
Sangwook Hwang <yoonseol1983@gmail.com>
2023-02-01 23:49:26 +0900

    16 - 스프링
    * 사용자 정의 application property 를 IDE 에서 편리하게 사용하기 위한 Spring Boot Configuration Processor 활용
    * 참고 : https://docs.spring.io/spring-boot/docs/current/reference/html/application-properties.html
    * decoupled logic 반영한 thymeleaf3 설정 및 view 구현 
    * 참고 : https://www.thymeleaf.org/doc/tutorials/3.1/usingthymeleaf.html#decoupled-template-logic

--- board/src/test/java/com/spring/board/controller/ArticleControllerTest.java
+++ board/src/test/java/com/spring/board/controller/ArticleControllerTest.java
@@ -1,4 +1,77 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.spring.board.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View Controller Test")
+@WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
-  
+
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Disabled
+    @DisplayName("[view] [GET] 게시글 리스트 페이지 ")
+    @Test
+    public void articleList() throws Exception {
+        // given
+
+        // when & then
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[view] [GET] 게시글 상세 페이지 - 정상 호출 ")
+    @Test
+    public void articleDetail() throws Exception {
+        // given
+
+        // when & then
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("article"));
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[view] [GET] 게시글 검색 전용 페이지 - 정상 호출 ")
+    @Test
+    public void articleSearch() throws Exception {
+        // given
+
+        // when & then
+        mvc.perform(get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("articles/search"))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[view] [GET] 게시글 해시태그 검색 페이지 - 정상 호출 ")
+    @Test
+    public void article() throws Exception {
+        // given
+
+        // when & then
+        mvc.perform(get("/articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("articles/search-hashtag"))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    }
 }
