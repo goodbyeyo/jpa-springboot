a397a8453193b054333a3e95a1db661ba5a998ee
Sangwook Hwang <yoonseol1983@gmail.com>
2023-03-14 21:43:11 +0900

    feat: main rebase 하기 위한 임시커밋

--- board/src/main/java/com/spring/board/dto/ArticleCommentDto.java
+++ board/src/main/java/com/spring/board/dto/ArticleCommentDto.java
@@ -6,17 +6,21 @@ import java.time.LocalDateTime;
  * A DTO for the {@link com.spring.board.domain.ArticleComment} entity
  */
 public record ArticleCommentDto(
+        Long id,
+        Long articleId,
         String content,
         LocalDateTime createdAt,
         String createdBy,
         LocalDateTime modifiedAt,
         String modifiedBy
 ) {
-    public static ArticleCommentDto of(String content,
+    public static ArticleCommentDto of(Long id,
+                                       Long articleId,
+                                       String content,
                                        LocalDateTime createdAt,
                                        String createdBy,
                                        LocalDateTime modifiedAt,
                                        String modifiedBy) {
-        return new ArticleCommentDto(content, createdAt, createdBy, modifiedAt, modifiedBy);
+        return new ArticleCommentDto(id, articleId, content, createdAt, createdBy, modifiedAt, modifiedBy);
     }
 }
