bb3fc63a18db342ed0275714e839e73f540cd496
Sangwook Hwang <yoonseol1983@gmail.com>
2023-03-17 23:57:50 +0900

    #21 - Article 및 ArticleComment API 및 View 구현 완료

--- board/src/main/java/com/spring/board/controller/ArticleController.java
+++ board/src/main/java/com/spring/board/controller/ArticleController.java
@@ -1,26 +1,56 @@
 package com.spring.board.controller;
 
+import com.spring.board.domain.Article;
+import com.spring.board.domain.constant.SearchType;
+import com.spring.board.dto.ArticleDto;
+import com.spring.board.dto.ArticleWithCommentsDto;
+import com.spring.board.dto.response.ArticleCommentResponse;
+import com.spring.board.dto.response.ArticleResponse;
+import com.spring.board.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @RequestMapping("/articles")
+@RequiredArgsConstructor
 @Controller
 public class ArticleController {
+
+    private final ArticleService articleService;
     @GetMapping
-    public String articles(ModelMap map ) {
-        map.addAttribute("articles", List.of());
+    public String articles(
+            @RequestParam(required = false) SearchType searchType,
+            @RequestParam(required = false) String searchValue,
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            ModelMap map ) {
+
+        var articles = articleService
+                .searchArticles(searchType, searchValue, pageable)
+                .map(ArticleResponse::from);
+
+        map.addAttribute("articles", articles);
         return "articles/index";
     }
 
     @GetMapping("/{articleId}")
     public String article(@PathVariable Long articleId, ModelMap map) {
-        map.addAttribute("article", "article"); // todo : 실제데이터 응답 필요
+        ArticleDto article = articleService.getArticle(articleId);
+        map.addAttribute("articles", ArticleResponse.from(article));
+//        ArticleCommentResponse.from()
+//        map.addAttribute("article", ArticleCommentResponse.from(article)); // todo : 실제데이터 응답 필요
+//        map.addAttribute("article", ArticleResponse.from(List.of()))
         map.addAttribute("articleComments", List.of());
         return "articles/detail";
     }
+
 }
--- board/src/main/java/com/spring/board/repository/ArticleRepository.java
+++ board/src/main/java/com/spring/board/repository/ArticleRepository.java
@@ -1,11 +1,9 @@
 package com.spring.board.repository;
 
 import com.querydsl.core.types.dsl.DateTimeExpression;
-import com.querydsl.core.types.dsl.SimpleExpression;
 import com.querydsl.core.types.dsl.StringExpression;
 import com.spring.board.domain.Article;
 import com.spring.board.domain.QArticle;
-import com.spring.board.dto.ArticleDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -34,5 +32,12 @@ public interface ArticleRepository extends
 //        bindings.bind(root.title).first(StringExpression::likeIgnoreCase);  // like '${value} %
     }
 
-    Page<ArticleDto> findByTitle(String title, Pageable pageable);
+    // Containing: like '%${value}%' 부분검색 가능 : 인덱스 사용 불가
+    Page<Article> findByTitleContaining(String title, Pageable pageable);
+    Page<Article> findByContentContaining(String content, Pageable pageable);
+    Page<Article> findByUserAccount_UserIdContaining(String userId, Pageable pageable);
+    Page<Article> findByUserAccount_NicknameContaining(String nickname, Pageable pageable);
+    Page<Article> findByHashtag(String hashtag, Pageable pageable);
+
+
 }
